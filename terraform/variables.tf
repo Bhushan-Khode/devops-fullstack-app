1247fc15d61526cbec06e171ea44984066910c3b
Bhushan-Khode <160607500+Bhushan-Khode@users.noreply.github.com>
2024-06-24 14:00:27 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "key_name" {
+  description = "Project.pem"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = ""C:\Users\asus\Downloads\Project.pem""
+  type        = string
+}
