fdbc3eb13bafce8c9a3c89583c63802d046738c9
Bhushan-Khode <160607500+Bhushan-Khode@users.noreply.github.com>
2024-06-24 22:19:58 +0530

    Delete terraform directory

--- terraform/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-key_name = "project.pem"
-private_key_path = ""C:\Users\asus\Downloads\Project.pem""
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "key_name" {
-  description = "Project.pem"
-  type        = string
-}
-
-variable "private_key_path" {
-  description = ""C:\Users\asus\Downloads\Project.pem""
-  type        = string
-}
